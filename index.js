aa576493b3a4ca3cd916cc4e426e9c8b0b2cc23a
jshepp27 <j.sheppard.ldn@gmail.com>
2020-04-18 12:01:48 +0100

    updatezzz

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({ bye: "buddy" });
 });
 
 app.post("/hello", (req, res) => {
