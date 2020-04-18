62ae04708d5453850ac7747bd3c37b982dfb5ae0
jshepp27 <j.sheppard.ldn@gmail.com>
2020-04-18 11:35:47 +0100

    heroku ready

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const express = require(express);
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
 });
+
+app.post("/hello", (req, res) => {
+  console.log(req.body);
+  console.log(req.body.name);
+});
+
 console.log("testing");
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
